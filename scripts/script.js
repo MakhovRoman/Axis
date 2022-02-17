b5ea404690119c34611d0333950160b2a0ddf976
MakhoRoman <nekromantos30888666@gmail.com>
2022-02-17 22:43:00 +0300

    fix banner annimation

--- scripts/script.js
+++ scripts/script.js
@@ -177,8 +177,89 @@ servicesButtonBlock.addEventListener('click', function() {
 
 
 //============= Маска телефона =============
-let contactsTel = document.querySelector('#contacts__tel');
+window.addEventListener('DOMContentLoaded', function() {
+  let inputs = document.querySelectorAll('input[type="tel"]');
 
+  Array.prototype.forEach.call(inputs, function(input) {
+    new InputMask({
+      selector: input, // в качестве селектора может быть элемент, или, собственно css селектор('#input', '.input', 'input'). Если селектор - тег или класс - будет получен только первый элемент
+      layout: input.dataset.mask
+    })
+  })
+
+})
+
+function InputMask(options) {
+    this.el = this.getElement(options.selector);
+    if (!this.el) return console.log('Что-то не так с селектором');
+    this.layout = options.layout || '+_ (___) ___-__-__';
+    this.maskreg = this.getRegexp();
+
+    this.setListeners();
+}
+
+InputMask.prototype.getRegexp = function() {
+    let str = this.layout.replace(/_/g, '\\d')
+    str = str.replace(/\(/g, '\\(')
+    str = str.replace(/\)/g, '\\)')
+    str = str.replace(/\+/g, '\\+')
+    str = str.replace(/\s/g, '\\s')
+
+    return str;
+}
+
+InputMask.prototype.mask = function(e) {
+    let _this = e.target,
+        matrix = this.layout,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = _this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) val = def;
+
+    _this.value = matrix.replace(/./g, function(a) {
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+
+    if (e.type == "blur") {
+        var regexp = new RegExp(this.maskreg);
+        if (!regexp.test(_this.value)) _this.value = "";
+    } else {
+        this.setCursorPosition(_this.value.length, _this);
+    }
+}
+
+InputMask.prototype.setCursorPosition = function(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+    }
+}
+
+InputMask.prototype.setListeners = function() {
+    this.el.addEventListener("input", this.mask.bind(this), false);
+    this.el.addEventListener("focus", this.mask.bind(this), false);
+    this.el.addEventListener("blur", this.mask.bind(this), false);
+}
+
+InputMask.prototype.getElement = function(selector) {
+    if (selector === undefined) return false;
+    if (this.isElement(selector)) return selector;
+    if (typeof selector == 'string') {
+        var el = document.querySelector(selector);
+        if (this.isElement(el)) return el;
+    }
+    return false
+}
+
+InputMask.prototype.isElement = function(element) {
+    return element instanceof Element || element instanceof HTMLDocument;
+}
 
 //============= Поле с именем и фамилией =============
 function noDigits(event) {
@@ -196,17 +277,36 @@ let headerTop = document.querySelector('.header__top');
 let headerBot = document.querySelector('.header__bot');
 let headerBotHeight = parseInt(getComputedStyle(headerBot).height);
 let headerTopHeight = parseInt(getComputedStyle(headerTop).height);
+let bannerHeight = parseInt(getComputedStyle(document.querySelector('.banner')).height);
 let headerPositionTop;
 let pageScroll;
+let headerLastScrollTop = 0;
+
 
 window.addEventListener('scroll', function() {
   pageScroll = window.pageYOffset;
+  let scrollTop = pageScroll;
 
+
+  /*
+  let timerId = setTimeout( () => {
+    if (pageScroll > headerTopHeight) {
+      headerPositionTop = headerBotHeight + headerTopHeight;
+      header.style.top = `${-headerPositionTop}px`;
+    } else {
+      return;
+    }
+  }, 5000);
+  */
   if (pageScroll > headerTopHeight) {
     headerPositionTop = headerTopHeight;
     header.style.top = `${-headerPositionTop}px`;
+
   } else {
-    header.style.top = '0';
+    /*clearTimeout(timerId);*/
+    headerPositionTop = 0;
+    header.style.top = `${-headerPositionTop}px`;
+
   }
 
   if (pageScroll > 265) {
@@ -215,7 +315,14 @@ window.addEventListener('scroll', function() {
     header.style.backgroundColor = 'transparent';
   }
 
+  if (pageScroll > bannerHeight) {
+    if (scrollTop > headerLastScrollTop) {
+      headerPositionTop = headerTopHeight + headerBotHeight;
+    } else {
+      headerPositionTop = headerTopHeight;
+    }
+    header.style.top = `${-headerPositionTop}px`;
+    headerLastScrollTop = scrollTop;
+  }
 
-
-  console.log(pageScroll);
 })
